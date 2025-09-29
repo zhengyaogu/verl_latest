19ffab631549ef87bfd89dfeb911dc1c634e33b0
Zhengyao Gu <zhengyao.gu30@gmail.com>
2025-09-29 02:47:58 +0000

    zebra

--- verl/utils/reward_score/__init__.py
+++ verl/utils/reward_score/__init__.py
@@ -16,6 +16,59 @@
 from verl.utils.import_utils import deprecated
 
 
+def remove_boxed(s):
+    left = "\\boxed{"
+    try:
+        assert s[:len(left)] == left
+        assert s[-1] == "}"
+        return s[len(left):-1]
+    except:
+        return None
+
+def last_boxed_only_string(string):
+    idx = string.rfind("\\boxed")
+    if idx < 0:
+        idx = string.rfind("\\fbox")
+        if idx < 0:
+            return None
+
+    i = idx
+    right_brace_idx = None
+    num_left_braces_open = 0
+    while i < len(string):
+        if string[i] == "{":
+            num_left_braces_open += 1
+        if string[i] == "}":
+            num_left_braces_open -= 1
+            if num_left_braces_open == 0:
+                right_brace_idx = i
+                break
+        i += 1
+    
+    if right_brace_idx == None:
+        retval = None
+    else:
+        retval = string[idx:right_brace_idx + 1]
+    
+    return retval
+
+def extract_solution(solution_str):
+    """Extract the equation from the solution string."""
+    # Remove everything before the first "Assistant:"
+    if "Assistant:" in solution_str:
+        solution_str = solution_str.split("Assistant:", 1)[1]
+    elif "<|im_start|>assistant" in solution_str:
+        solution_str = solution_str.split("<|im_start|>assistant", 1)[1]
+    else:
+        return None
+    
+    solution_str = solution_str.split('\n')[-1]
+
+    
+    final_answer = remove_boxed(last_boxed_only_string(solution_str))
+    return final_answer
+
+
 def default_compute_score(
     data_source,
     solution_str,
---
+++ verl/workers/reward_manager/zebra.py
@@ -0,0 +1,64 @@
+import re
+import random
+from verl.utils.reward_score import extract_solution
+
+
+
+
+def evaluate_equation(answer, target):
+    """Safely evaluate the arithmetic equation using eval() with precautions."""
+    if answer == None:
+        return False
+    if answer.lower().strip() != target.lower().strip():
+        return False
+    else:
+        return True
+
+
+
+def compute_score(solution_str, ground_truth, method='strict', format_score=0.1, score=1.):
+    """The scoring function for countdown task.
+    
+    Args:
+        solution_str: the solution text
+        ground_truth: dictionary containing target number and available numbers
+        method: the method to extract the solution
+        format_score: the score for correct format but wrong answer
+        score: the score for the correct answer
+    """
+    target = ground_truth['target']
+    
+    equation = extract_solution(solution_str=solution_str)
+    do_print = random.randint(1, 64) == 1
+    
+    if do_print:
+        print(f"--------------------------------")
+        print(f"Target: {target}")
+        print(f"Extracted equation: {equation}")
+        print(f"Solution string: {solution_str}")
+
+    if equation is None:
+        if do_print:
+            print(f"No equation found")
+        return 0
+    
+        
+    # Evaluate equation
+    try:
+        result = evaluate_equation(equation, target)
+
+        if result:
+            if do_print:
+                print(f"Correct equation: {equation} = {result}")
+            return score
+        else:
+            if do_print:
+                print(f"Wrong result: equation = {equation}, target = {target}")
+            return format_score
+    except:
+        if do_print:
+            print(f"Error evaluating equation")
+        return format_score 
+    
+def get_zebra_compute_score(correct_score, format_score):
+    return lambda solution_str, ground_truth: compute_score(solution_str, ground_truth, method='strict', format_score=format_score, score=correct_score)
